f1793078d05f5452c7af716464bfe9e54836b5e3
Nazhwa <najeu05@gmail.com>
2025-09-15 11:22:44 +0700

    update

--- src/components/profile/ProfileInfoItem.jsx
+++ src/components/profile/ProfileInfoItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ProfileInfoItem = ({ icon: content }) => {
+const ProfileInfoItem = ({ icon: Icon, content }) => {
   return (
     <div className="flex items-center gap-2 text-gray-600">
       <Icon size={18} />
